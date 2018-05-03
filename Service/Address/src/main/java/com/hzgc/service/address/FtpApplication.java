c32db3e3e3b5411a1eb415457a49b253b7af226a
lemon <lemon52121@163.com>
2018-05-03 17:26:30 +0800

    address 代码调整

--- Service/Address/src/main/java/com/hzgc/service/address/FtpApplication.java
+++ Service/Address/src/main/java/com/hzgc/service/address/FtpApplication.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
-@PropertySource("clusteringApplication.properties")
+@PropertySource("ftpApplication.properties")
 public class FtpApplication {
 
     public static void main(String[] args) {
