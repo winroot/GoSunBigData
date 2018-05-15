f5d8082705360948f2e7097be179bb45ec2d53e2
zhaozhe <dgxdks@163.com>
2018-05-15 15:15:32 +0800

    modify

---
+++ Cluster/Spark/src/main/java/com/hzgc/cluster/spark/spark/starepo/JDBCProperties.java
@@ -0,0 +1,54 @@
+package com.hzgc.cluster.spark.spark.starepo;
+
+import com.hzgc.common.util.file.ResourceFileUtil;
+import com.hzgc.common.util.properties.ProperHelper;
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Properties;
+
+public class JDBCProperties extends ProperHelper implements Serializable {
+
+    private static Logger LOG = Logger.getLogger(JDBCProperties.class);
+
+    private static Properties props = new Properties();
+    private static String hiveJDBCURL;
+    private static String phoenixJDBCURL;
+
+    static {
+        String properName = "phoenixJdbc.properties";
+        FileInputStream in = null;
+        try {
+            props.load(ResourceFileUtil.loadResourceInputStream(properName));
+            setHiveJDBCURL();
+            setPhoenixJDBCURL();
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOG.error("Catch an unknown error, can't load the configuration file" + properName);
+        }
+    }
+
+    private static void setHiveJDBCURL() {
+        JDBCProperties.hiveJDBCURL = verifyCommonValue("url",
+                "jdbc:hive2://s103:2181,s104:2181,s108:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=thriftserver",
+                props, LOG);
+    }
+
+    private static void setPhoenixJDBCURL() {
+        JDBCProperties.phoenixJDBCURL = verifyCommonValue("phoenixJDBCURL", "jdbc:phoenix:host_replace:2181", props, LOG);
+    }
+
+    public static Properties getProps() {
+        return props;
+    }
+
+    public static String getHiveJDBCURL() {
+        return hiveJDBCURL;
+    }
+
+    public static String getPhoenixJDBCURL() {
+        return phoenixJDBCURL;
+    }
+}
---
+++ Cluster/Spark/src/main/scala/com/hzgc/cluster/spark/spark/alarm/FaceAddAlarmJob.scala
@@ -0,0 +1,121 @@
+package com.hzgc.cluster.spark.spark.alarm
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.google.gson.Gson
+import com.hzgc.cluster.spark.spark.device.DeviceUtilImpl
+import com.hzgc.cluster.spark.spark.message.AddAlarmMessage
+import com.hzgc.cluster.spark.spark.rocmq.RocketMQProducer
+import com.hzgc.cluster.spark.spark.starepo.ObjectInfoInnerHandlerImpl
+import com.hzgc.cluster.spark.spark.util.{FaceObjectUtil, PropertiesUtils}
+import com.hzgc.common.table.device.DeviceTable
+import com.hzgc.jni.FaceFunction
+import kafka.serializer.StringDecoder
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Durations, StreamingContext}
+
+import scala.collection.JavaConverters
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * 人脸新增告警实时处理任务（刘善彬）
+  */
+object FaceAddAlarmJob {
+
+  case class Json(staticID: String,
+                  staticObjectType: String,
+                  sim: Float)
+
+  def main(args: Array[String]): Unit = {
+    val deviceUtilI = new DeviceUtilImpl()
+    val properties = PropertiesUtils.getProperties
+    val appName = properties.getProperty("job.addAlarm.appName")
+    val timeInterval = Durations.seconds(properties.getProperty("job.addAlarm.timeInterval").toLong)
+    val conf = new SparkConf()
+      .setAppName(appName)
+    val ssc = new StreamingContext(conf, timeInterval)
+    val kafkaGroupId = properties.getProperty("kafka.FaceAddAlarmJob.group.id")
+    val topics = Set(properties.getProperty("kafka.topic.name"))
+    val mqTopic = properties.getProperty("rocketmq.topic.name")
+    val nameServer = properties.getProperty("rocketmq.nameserver")
+    val grouId = properties.getProperty("rocketmq.group.id")
+    val brokers = properties.getProperty("kafka.metadata.broker.list")
+    val kafkaParams = Map(
+      "metadata.broker.list" -> brokers,
+      "group.id" -> kafkaGroupId
+    )
+    val df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val kafkaDynamicPhoto = KafkaUtils.
+      createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
+
+    val jsonResult = kafkaDynamicPhoto.map(data => (data._1, FaceObjectUtil.jsonToObject(data._2)))
+      .filter(obj => obj._2.getAttribute.getFeature != null && obj._2.getAttribute.getFeature.length == 512)
+      .map(obj => {
+      val totalList = JavaConverters.
+        asScalaBufferConverter(ObjectInfoInnerHandlerImpl.getInstance().getTotalList).asScala
+      val faceObj = obj._2
+      val ipcID = faceObj.getIpcId
+      val platID = deviceUtilI.getplatfromID(ipcID)
+      val alarmRule = deviceUtilI.isWarnTypeBinding(ipcID)
+      val filterResult = new ArrayBuffer[Json]()
+      if (platID != null && platID.length > 0) {
+        if (alarmRule != null && !alarmRule.isEmpty) {
+          val addWarnRule = alarmRule.get(DeviceTable.ADDED)
+          if (addWarnRule != null && !addWarnRule.isEmpty) {
+            totalList.foreach(record => {
+              if (addWarnRule.containsKey(record(1))) {
+                val threshold = FaceFunction.featureCompare(record(2).asInstanceOf[Array[Float]], faceObj.getAttribute.getFeature)
+                if (threshold > addWarnRule.get(record(1))) {
+                  filterResult += Json(record(0).asInstanceOf[String], record(1).asInstanceOf[String], threshold)
+                }
+              }
+            })
+            val finalResult = filterResult.sortWith(_.sim > _.sim).take(3)
+            (obj._2, ipcID, platID, finalResult)
+          } else {
+            println("Device [" + ipcID + "] does not bind added alarm rule,current time [" + df.format(new Date()) + "]")
+            (obj._2, ipcID, null, filterResult)
+          }
+        } else {
+          println("Device [" + ipcID + "] does not bind alarm rule,current time [" + df.format(new Date()) + "]")
+          (obj._2, ipcID, null, filterResult)
+        }
+      } else {
+        println("Device [" + ipcID + "] does not bind platform ID,current time [" + df.format(new Date()) + "]")
+        (obj._2, ipcID, null, filterResult)
+      }
+    }).filter(jsonResultFilter => jsonResultFilter._3 != null)
+
+    jsonResult.foreachRDD(resultRDD => {
+      resultRDD.foreachPartition(parRDD => {
+        val rocketMQProducer = RocketMQProducer.getInstance(nameServer, mqTopic, grouId)
+        val gson = new Gson()
+        parRDD.foreach(result => {
+          //识别集合为null，对该条数据进行新增告警。
+          if (result._4 == null || result._4.isEmpty) {
+            val dateStr = df.format(new Date())
+            val addAlarmMessage = new AddAlarmMessage()
+            val surl = result._1.getSurl
+            addAlarmMessage.setAlarmTime(dateStr)
+            addAlarmMessage.setAlarmType(DeviceTable.ADDED.toString)
+
+            addAlarmMessage.setSmallPictureURL(surl)
+            addAlarmMessage.setBigPictureURL(result._1.getBurl)
+            addAlarmMessage.setDynamicDeviceID(result._2)
+            addAlarmMessage.setHostName(result._1.getHostname)
+            rocketMQProducer.send(result._3,
+              "alarm_" + DeviceTable.ADDED.toString,
+              surl,
+              gson.toJson(addAlarmMessage).getBytes(),
+              null)
+          }
+        })
+      })
+    })
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
