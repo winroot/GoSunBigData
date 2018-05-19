505979c80c4f58d1a5f22b1999d819c0b4e5c799
zhaozhe <dgxdks@163.com>
2018-05-19 09:41:07 +0800

    modify

--- Service/DynRepo/src/main/java/com/hzgc/service/dynrepo/bean/SingleCaptureResult.java
+++ Service/DynRepo/src/main/java/com/hzgc/service/dynrepo/bean/SingleCaptureResult.java
@@ -7,20 +7,8 @@ import java.util.List;
 
 @Data
 public class SingleCaptureResult implements Serializable {
-
-    //原图片ID
-    private List<String> imageNames;
-
-    //单一结果的结果总数
+    private String deviceId;
+    private String deviceName;
     private int total;
-
-    /**
-     * 非设备归类时的结果集
-     * 在第一次查询返回结果是肯定是按此种集合返回
-     * 后续再次查询时如果按照设备归类的则有可能按照picturesByIpc来返回
-     */
     private List<CapturedPicture> pictures;
-
-    //按设备归类时的结果集
-    private List<GroupByIpc> devicePictures;
 }
