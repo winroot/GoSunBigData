6ee12d829356441fce12c6ac5e2bc00b3081b7b6
zhaozhe <dgxdks@163.com>
2018-06-06 17:32:43 +0800

    modify

--- Service/Visual/src/main/java/com/hzgc/service/visual/controller/PeopleManagerController.java
+++ Service/Visual/src/main/java/com/hzgc/service/visual/controller/PeopleManagerController.java
@@ -1,9 +1,12 @@
 package com.hzgc.service.visual.controller;
 
+import com.hzgc.service.util.error.RestErrorCode;
 import com.hzgc.service.util.response.ResponseResult;
 import com.hzgc.service.util.rest.BigDataPath;
 import com.hzgc.service.visual.service.PeopleManagerService;
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "人口管理服务")
+@Slf4j
 public class PeopleManagerController {
     @Autowired
     private PeopleManagerService peopleManagerService;
 
-    @RequestMapping(value = BigDataPath.STAREPO_COUNT_MIGRATION, method = RequestMethod.GET)
-    public ResponseResult
+    @RequestMapping(value = BigDataPath.STAREPO_COUNT_EMIGRATION, method = RequestMethod.GET)
+    public ResponseResult<Object> peopleCount(String start_time, String end_time) {
+        if (StringUtils.isBlank(start_time) || StringUtils.isBlank(end_time)) {
+            log.error("Start people count, but start_time or end_time is error");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT);
+        }
+        peopleManagerService.peopleCount(start_time, end_time);
+    }
 }
--- Service/Visual/src/main/java/com/hzgc/service/visual/service/PeopleManagerService.java
+++ Service/Visual/src/main/java/com/hzgc/service/visual/service/PeopleManagerService.java
@@ -1,10 +1,50 @@
 package com.hzgc.service.visual.service;
 
+import com.hzgc.service.starepo.bean.export.EmigrationCount;
+import com.hzgc.service.util.rest.BigDataPath;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
 
 @Service
 @Slf4j
 public class PeopleManagerService {
+    @Autowired
+    private RestTemplate restTemplate;
 
+    public Object peopleCount(String start_time, String end_time) {
+        List<EmigrationCount> emigrationCounts = emigrationCount(start_time, end_time);
+        return null;
+    }
+
+    @HystrixCommand(fallbackMethod = "emigrationCountError")
+    private List<EmigrationCount> emigrationCount(String start_time, String end_time) {
+        Map<String, String> keyValue = new HashMap<>();
+        keyValue.put("start_time", start_time);
+        keyValue.put("end_time", end_time);
+        EmigrationCount[] countArr = restTemplate.getForObject(
+                "http://starepo/" + BigDataPath.STAREPO_COUNT_EMIGRATION,
+                EmigrationCount[].class,
+                keyValue
+        );
+        if (countArr != null) {
+            return Arrays.asList(countArr);
+        } else {
+            return null;
+        }
+    }
+
+    private List<EmigrationCount> emigrationCountError(String start_time, String end_time) {
+        log.info("Emigration count faild");
+        return null;
+    }
 }
