87c182ca1f1472362ebe61c3406e64641d1c726b
zhaozhe <dgxdks@163.com>
2018-06-06 11:25:10 +0800

    modify

--- Cluster/ES/bin/dynamic-index.sh
+++ Cluster/ES/bin/dynamic-index.sh
@@ -31,69 +31,70 @@ curl -XPUT 's100:9200/dynamic?pretty' -H 'Content-Type: application/json' -d'
     },
     "mappings": {
          "person": {
-              "properties": {
-                    "ftpurl" : {
-                                          "type" : "text"
-                              },
-                              "eyeglasses" : {
-                                           "type" : "long"
-                               },
-                              "gender" : {
-                                           "type" : "long"
-                              },
-                              "haircolor" : {
-                                           "type" : "long"
-                              },
-                              "hairstyle" : {
-                                           "type" : "long"
-                              },
-                              "hat" : {
-                                           "type" : "long"
-                              },
-                              "huzi" : {
-                                          "type" : "long"
-                              },
-                              "tie" : {
-                                           "type" : "long"
-                              },
-                              "ipcid" : {
-                                           "type" : "text"
-                              },
-                              "timeslot" : {
-                                           "type" : "long"
-                              },
-                              "date" : {
-                                            "type" : "text"
-                              },
-                              "exacttime" : {
-                                  "type" : "date",
-                                  "format": "yyyy-MM-dd HH:mm:ss"
-                              },
-                              "searchtype" : {
-                                  "type" : "text"
-                              },
-                              "clusterid" : {
-                                  "type" : "text"
-                              },
-                              "alarmid" : {
-                                  "type" : "long"
-                              },
-                              "alarmtime" : {
-                                  "type" : "text"
-                              }
+         		"properties": {
+         			"ftpurl": {
+         				"type": "text"
+         			},
+         			"eyeglasses": {
+         				"type": "long"
+         			},
+         			"gender": {
+         				"type": "long"
+         			},
+         			"haircolor": {
+         				"type": "long"
+         			},
+         			"hairstyle": {
+         				"type": "long"
+         			},
+         			"hat": {
+         				"type": "long"
+         			},
+         			"huzi": {
+         				"type": "long"
+         			},
+         			"tie": {
+         				"type": "long"
+         			},
+         			"ipcid": {
+         				"type": "text",
+         				"fields": {
+         					"keyword": {
+         						"type": "keyword"
+         					}
+         				}
+         			},
+         			"timeslot": {
+         				"type": "long"
+         			},
+         			"date": {
+         				"type": "text"
+         			},
+         			"exacttime": {
+         				"type": "date",
+         				"format": "yyyy-MM-dd HH:mm:ss"
+         			},
+         			"searchtype": {
+         				"type": "text"
+         			},
+         			"clusterid": {
+         				"type": "text"
+         			},
+         			"alarmid": {
+         				"type": "long"
+         			},
+         			"alarmtime": {
+                    	"type": "text",
+                    	"fields": {
+                    		"keyword": {
+                    			"type": "keyword"
+                    		}
+                    	}
                     }
-              }
+         		}
+         	}
         }
-    }
-'
-curl -XPUT 's100:9200/dynamic/_mapping/person?pretty' -H 'Content-Type: application/json' -d'
-{
-  "alarmtime": {
-      "type":     "text",
-      "fielddata": true
-    }
-}
-'
+    }'
 
 curl -XPUT 's100:9200/dynamic/_settings' -d '{
     "index": {
