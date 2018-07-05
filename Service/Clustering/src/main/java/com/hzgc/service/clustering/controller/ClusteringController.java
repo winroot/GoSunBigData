0e1fd3e7015b7455cdba2aa0fa77c0ad5a3cac67
lsy521 <lemon52121@163.com>
2018-07-05 17:27:19 +0800

    modif

--- Service/Clustering/src/main/java/com/hzgc/service/clustering/controller/ClusteringController.java
+++ Service/Clustering/src/main/java/com/hzgc/service/clustering/controller/ClusteringController.java
@@ -13,7 +13,6 @@ import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
