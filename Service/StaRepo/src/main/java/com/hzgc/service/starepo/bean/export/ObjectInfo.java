e731c6fc4906955a2076341dea7c2ca0fd341ff2
lsy521 <lemon52121@163.com>
2018-07-06 15:35:58 +0800

    xiu gai zi duan

--- Service/StaRepo/src/main/java/com/hzgc/service/starepo/bean/export/ObjectInfo.java
+++ Service/StaRepo/src/main/java/com/hzgc/service/starepo/bean/export/ObjectInfo.java
@@ -10,17 +10,17 @@ import lombok.Data;
 public class ObjectInfo {
     /* 根据id获取对象信息——后台返回封装 */
     @ApiModelProperty(value = "对象名字")
-    private String name;                        // 对象名字
+    private String name;                       // 对象名字
     @ApiModelProperty(value = "对象类型名字")
     private String objectTypeName;             // 对象类型名字
     @ApiModelProperty(value = "身份证")
     private String idCardNumber;               // 身份证
     @ApiModelProperty(value = "性别")
-    private int sex;                            // 性别 [ 1:男，2:女 默认为0：未知]
+    private Integer sex;                       // 性别 [0 = 未知（默认选项），1 = 男，2 = 女]
     @ApiModelProperty(value = "布控理由")
     private String createdReason  ;            // 布控理由
     @ApiModelProperty(value = "创建人")
-    private String creator;                     // 创建人
+    private String creator;                    // 创建人
     @ApiModelProperty(value = "布控人联系方式")
     private String creatorPhone;               // 布控人联系方式
     @ApiModelProperty(value = "创建时间")
@@ -28,7 +28,7 @@ public class ObjectInfo {
     @ApiModelProperty(value = "更新时间")
     private String updateTime;                 // 更新时间
     @ApiModelProperty(value = "关注等级")
-    private int followLevel;                   // 关注等级 [ 0:非重点关注，1:重点关注，默认为0]
+    private Integer followLevel;               // 关注等级 [1 = 非重点关注（默认选项），2 = 重点关注]
     @ApiModelProperty(value = "人员状态")
-    private int status;                         // 人员状态 [ 0:常住人口，1:建议迁出，默认为0]
+    private int status;                        // 人员状态 [0 = 常住人口（默认选项），1 = 建议迁出]
 }
